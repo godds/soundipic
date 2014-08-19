f6fc905a72c411a62399e7815fb1b60e622056ad
Graham Odds <godds@scottlogic.co.uk>
2014-08-19 16:56:47 +0100

    added volume control

--- src/app/sound/sound.js
+++ src/app/sound/sound.js
@@ -1,5 +1,6 @@
 angular.module("soundipic.sound", [
   "ui.router",
+  "audio",
   "audio.soundipic",
   "soundipic.model"
 ])
@@ -16,9 +17,15 @@ angular.module("soundipic.sound", [
   });
 })
 
-.controller("SoundCtrl", function SoundController($scope, model, soundipic) {
+.controller("SoundCtrl", function SoundController($scope, model, audio, soundipic) {
     $scope.playing = false;
     $scope.imageSrc = model.imageSrc();
+    $scope.volume = audio.masterVolume();
+
+    function adjustVolume(delta) {
+      audio.masterVolume(audio.masterVolume() + delta);
+      $scope.volume = audio.masterVolume();
+    }
 
     $scope.togglePlaying = function() {
       if ($scope.playing) {
@@ -29,6 +36,13 @@ angular.module("soundipic.sound", [
         soundipic.play(model.imageData());
         $scope.playing = true;
       }
+    };
+
+    $scope.volumeDown = function() {
+      adjustVolume(-0.1);
+    };
+    $scope.volumeUp = function() {
+      adjustVolume(0.1);
     };
 })
 
